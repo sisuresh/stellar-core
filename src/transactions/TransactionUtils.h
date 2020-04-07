3c05d3a3bc1daf09db86dc3866bcd1f4530d0850
marta-lokhova <marta@stellar.org>
2020-04-06 18:30:14 -0700

    Allow sorting signers for AccountEntry

--- src/transactions/TransactionUtils.h
+++ src/transactions/TransactionUtils.h
@@ -131,6 +131,7 @@ bool isAuthRequired(ConstLedgerTxnEntry const& entry);
 bool isImmutableAuth(LedgerTxnEntry const& entry);
 
 void normalizeSigners(LedgerTxnEntry& entry);
+void normalizeSigners(AccountEntry& acc);
 
 void releaseLiabilities(AbstractLedgerTxn& ltx, LedgerTxnHeader const& header,
                         LedgerTxnEntry const& offer);
