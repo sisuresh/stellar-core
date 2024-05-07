f00bb89a982597c138f538ea9b4bfaf8dece9dc0
Garand Tyson <garand@stellar.org>
2024-05-07 16:35:21 -0700

    Cleanup for release builds

--- src/transactions/TransactionBridge.h
+++ src/transactions/TransactionBridge.h
@@ -22,6 +22,8 @@ namespace txbridge
 
 TransactionEnvelope convertForV13(TransactionEnvelope const& input);
 
+xdr::xvector<DecoratedSignature, 20> const&
+getSignatures(TransactionEnvelope const& env);
 xdr::xvector<DecoratedSignature, 20>& getSignatures(TransactionEnvelope& env);
 xdr::xvector<DecoratedSignature, 20>&
 getSignaturesInner(TransactionEnvelope& env);
