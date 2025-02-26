83e960f8df7b77ae1badb9cea304103a6e0287c4
Siddharth Suresh <siddharth@stellar.org>
2025-02-26 14:33:54 -0800

    Stop throwing exceptions on unrecoverable errors

--- src/ledger/LedgerManagerImpl.cpp
+++ src/ledger/LedgerManagerImpl.cpp
@@ -1827,10 +1827,9 @@ LedgerManagerImpl::applyThread(
                 auto it = entryMap->find(ttlKey);
                 if (it != entryMap->end())
                 {
-                    releaseAssertOrThrow(it->second.mLedgerEntry);
-                    releaseAssertOrThrow(it->second.mLedgerEntry->data.ttl()
-                                             .liveUntilLedgerSeq <=
-                                         extIt->second);
+                    releaseAssert(it->second.mLedgerEntry);
+                    releaseAssert(it->second.mLedgerEntry->data.ttl()
+                                      .liveUntilLedgerSeq <= extIt->second);
 
                     it->second.mLedgerEntry->data.ttl().liveUntilLedgerSeq =
                         extIt->second;
@@ -1879,13 +1878,13 @@ LedgerManagerImpl::applyThread(
                 releaseAssert(it != entryMap->end());
 
                 auto isReadOnlyTTLIt = isReadOnlyTTLMap.find(lk);
-                releaseAssertOrThrow(lk.type() != TTL ||
-                                     isReadOnlyTTLIt != isReadOnlyTTLMap.end());
+                releaseAssert(lk.type() != TTL ||
+                              isReadOnlyTTLIt != isReadOnlyTTLMap.end());
 
                 if (lk.type() == TTL && it->second.mLedgerEntry && updatedLe &&
                     isReadOnlyTTLIt->second /*isReadOnly*/)
                 {
-                    releaseAssertOrThrow(
+                    releaseAssert(
                         updatedLe->data.ttl().liveUntilLedgerSeq >
                         it->second.mLedgerEntry->data.ttl().liveUntilLedgerSeq);
 
@@ -1913,18 +1912,18 @@ LedgerManagerImpl::applyThread(
             for (auto const& key : res.getRestoredKeys().hotArchive)
             {
                 auto [_, inserted] = threadRestoredKeys.hotArchive.emplace(key);
-                releaseAssertOrThrow(inserted);
+                releaseAssert(inserted);
             }
             for (auto const& key : res.getRestoredKeys().liveBucketList)
             {
                 auto [_, inserted] =
                     threadRestoredKeys.liveBucketList.emplace(key);
-                releaseAssertOrThrow(inserted);
+                releaseAssert(inserted);
             }
         }
         else
         {
-            releaseAssertOrThrow(!txBundle.getResPayload().isSuccess());
+            releaseAssert(!txBundle.getResPayload().isSuccess());
         }
     }
 
